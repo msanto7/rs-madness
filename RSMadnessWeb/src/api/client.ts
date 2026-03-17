491bf1e620adc923a2af4b73385ac90e245aa160
msanto7 <mike.santoro42@gmail.com>
2026-03-17 00:29:27 -0400

    axios setup

---
+++ RSMadnessWeb/src/api/client.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+// axios calls will use this base URL
+const apiClient = axios.create({
+    baseURL: 'http://localhost:5202/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// interceptor for outgoing requests to the API
+apiClient.interceptors.request.use((config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}));
+
+export default apiClient;
