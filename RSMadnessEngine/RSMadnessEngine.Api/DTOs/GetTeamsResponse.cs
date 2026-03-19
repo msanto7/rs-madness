f6272287e1427ca1ad0f135a8f6b285d6359228f
msanto7 <mike.santoro42@gmail.com>
2026-03-19 00:52:15 -0400

    simple GET endpoint for the list of teams to be ranked by the user

---
+++ RSMadnessEngine/RSMadnessEngine.Api/DTOs/GetTeamsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace RSMadnessEngine.Api.DTOs
+{
+    public class GetTeamsResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Seed { get; set; }
+        public string Region { get; set; } = string.Empty;
+    }
+}
