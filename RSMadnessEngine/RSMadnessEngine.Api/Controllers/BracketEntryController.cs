27cd20d476d8f7f7514ed670f4d454deaca11914
msanto7 <mike.santoro42@gmail.com>
2026-03-19 01:47:28 -0400

    update/save endpoint for a bracket entry - NOT TESTED YET

--- RSMadnessEngine/RSMadnessEngine.Api/Controllers/BracketEntryController.cs
+++ RSMadnessEngine/RSMadnessEngine.Api/Controllers/BracketEntryController.cs
@@ -5,6 +5,7 @@ using RSMadnessEngine.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.EntityFrameworkCore;
+using RSMadnessEngine.Data.Entities;
 
 namespace RSMadnessEngine.Api.Controllers
 {
@@ -54,6 +55,120 @@ namespace RSMadnessEngine.Api.Controllers
             }
                 
             return Ok(entry);
+        }
+
+        /// <summary>
+        /// Creates or updates a bracket entry for the logged in user if valid.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>GetBracketEntryResponse object</returns>
+        [HttpPut("me/ranks")]
+        public async Task<ActionResult<GetBracketEntryResponse>> SaveRanks(SaveRanksRequest request)
+        {
+            var userId = GetUserId();
+
+            // make sure bracket ranking is valid
+            var errors = ValidateRanks(request.Ranks);
+            if (errors.Any())
+            {
+                return BadRequest(new { Errors = errors });
+            }
+
+            var bracketEntry = await _dbContext.BracketEntries
+                .Include(be => be.EntryTeamRanks)
+                .FirstOrDefaultAsync(be => be.UserId == userId);
+
+            // make sure we haven't locket in the bracket yet
+            if (bracketEntry != null && bracketEntry.SubmittedAt != null)
+            {
+                return BadRequest("Bracket entry has already been submitted and cannot be modified.");
+            }
+
+            // add a new entry if the user has not made one yet
+            if (bracketEntry == null)
+            {
+                bracketEntry = new BracketEntry
+                {
+                    UserId = userId,
+                    CreatedAt = DateTime.UtcNow
+                };
+                _dbContext.BracketEntries.Add(bracketEntry);
+            }
+
+            // overwrite the existing bracket state
+            bracketEntry.EntryTeamRanks.Clear();
+            foreach (var rank in request.Ranks)
+            {
+                bracketEntry.EntryTeamRanks.Add(new BracketEntryTeamRank
+                {
+                    TeamId = rank.TeamId,
+                    Rank = rank.Rank
+                });
+            }
+            await _dbContext.SaveChangesAsync();
+
+            // return a full new copy of the bracket entry state
+            var response = await _dbContext.BracketEntries
+                .Where(be => be.UserId == userId)
+                .Select(be => new GetBracketEntryResponse
+                {
+                    Id = be.Id,
+                    SubmittedAt = be.SubmittedAt,
+                    Ranks = be.EntryTeamRanks.Select(r => new TeamRankDTO
+                    {
+                        TeamId = r.TeamId,
+                        TeamName = r.Team.Name,
+                        Seed = r.Team.Seed,
+                        Region = r.Team.Region,
+                        Rank = r.Rank
+                    }).ToList()
+                }).FirstOrDefaultAsync();
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Method to validate the bracket rankong rules before saving
+        /// </summary>
+        /// <param name="ranks"></param>
+        /// <returns></returns>
+        private List<string> ValidateRanks(List<RankAssignment> ranks)
+        {
+            var errors = new List<string>();
+
+            // validate rank count
+            if (ranks.Count != 64)
+            {
+                errors.Add("Exactly 64 ranks must be assigned.");
+                return errors;
+            }
+
+            // validate rank values
+            var rankValues = ranks.Select(r => r.Rank).ToList();
+
+            if (rankValues.Any(r => r < 1 || r > 64))
+            {
+                errors.Add("Ranks must be between 1 and 64.");
+            }
+
+            if (rankValues.Distinct().Count() != 64)
+            {
+                errors.Add("Ranks must be unique.");
+            }
+
+            if (rankValues.Sum() != 2080)
+            {
+                errors.Add("Ranks must sum to 2080");
+            }
+
+            // validate teams
+            var teamIds = ranks.Select(r => r.TeamId).ToList();
+            if (teamIds.Distinct().Count() != 64)
+            {
+                errors.Add("Duplicate team Ids found.");
+            }
+
+            return errors;
         }
     }
 }
---
+++ RSMadnessEngine/RSMadnessEngine.Api/DTOs/BracketEntry/SaveRanksRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace RSMadnessEngine.Api.DTOs.BracketEntry
+{
+    public class SaveRanksRequest
+    {
+        public List<RankAssignment> Ranks { get; set; } = new List<RankAssignment>();
+    }
+
+    public class RankAssignment
+    {
+        public int TeamId { get; set; }
+        public int Rank { get; set; }
+    }
+}
