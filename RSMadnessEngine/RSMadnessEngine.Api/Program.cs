5a5ea0057e98e304e1cedca3cd3d7abe8a61ba0b
msanto7 <mike.santoro42@gmail.com>
2026-03-20 23:38:53 -0400

    temporarily disabling bg job to see if schema will run on prod db

--- RSMadnessEngine/RSMadnessEngine.Api/Program.cs
+++ RSMadnessEngine/RSMadnessEngine.Api/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IScoringService, ScoringService>();
 builder.Services.AddScoped<ISyncService, SyncService>();
 builder.Services.AddHttpClient<INcaaDataProvider, NcaaDataProvider>();
-builder.Services.AddHostedService<TournamentSyncBackgroundJob>();
+// builder.Services.AddHostedService<TournamentSyncBackgroundJob>();
 
 // jwt validation in request pipeline
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
